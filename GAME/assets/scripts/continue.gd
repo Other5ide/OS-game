39d9894829d6e48e53d735c3734d80f18677b30a
Other5ide <124216748+Other5ide@users.noreply.github.com>
2023-03-05 21:32:50 -0300

    Add files via upload

--- GAME/assets/scripts/continue.gd
+++ GAME/assets/scripts/continue.gd
@@ -1,4 +1,4 @@
-extends AnimationPlayer
+extends Button
 
 
 # Declare member variables here. Examples:
@@ -8,11 +8,6 @@ extends AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
-
-
-func _on_introplayer_animation_finished(_anim_name):
-	get_tree().change_scene("res://assets/scenes/menu.tscn")
-	pass # Replace with function body.
+#	pass
